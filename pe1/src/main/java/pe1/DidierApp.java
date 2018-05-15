7f6d06fd09a9cdf7e2202d8650993410d2f09b28
Defrance Didier <d2f@gmail.com>
2018-05-15 10:14:47 +0200

    bbb

--- pe1/src/main/java/pe1/DidierApp.java
+++ pe1/src/main/java/pe1/DidierApp.java
@@ -3,7 +3,7 @@ package pe1;
 public class DidierApp {
 
 	public static void main(String[] args) {
-		System.out.println("didier app v1 *** ###");
+		System.out.println("didier app v1 *** ### ***");
 
 	}
 
