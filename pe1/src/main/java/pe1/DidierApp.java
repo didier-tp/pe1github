ee4d6911ccb5ef768c7f8e268060c7e87daa3f15
Defrance Didier <d2f@gmail.com>
2018-05-15 11:10:14 +0200

    ***

--- pe1/src/main/java/pe1/DidierApp.java
+++ pe1/src/main/java/pe1/DidierApp.java
@@ -5,6 +5,7 @@ public class DidierApp {
 	public static void main(String[] args) {
 		System.out.println("didier app v1 *** ### ***");
         System.out.println("6 aout");
+        System.out.println("***");
 	}
 
 }
