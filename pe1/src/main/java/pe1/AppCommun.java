21a4850cf6534dc35f77b0fb2919c833ffdb9417
Defrance Didier <d2f@gmail.com>
2018-05-15 11:22:19 +0200

    correction bug en urgence sur master

--- pe1/src/main/java/pe1/AppCommun.java
+++ pe1/src/main/java/pe1/AppCommun.java
@@ -5,7 +5,7 @@ public class AppCommun {
 	public static void main(String[] args) {
 		System.out.println("app commun abdallah");
 		System.out.println("app commun didier");
-
+        System.out.println("correction bug en urgence");
 	}
 
 }
