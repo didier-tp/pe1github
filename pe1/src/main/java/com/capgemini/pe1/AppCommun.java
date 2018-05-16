ee9bc9b021c39bff96deaab0db1bdf7230267379
Defrance Didier <d2f@gmail.com>
2018-05-16 13:56:33 +0200

    avec moins de println

--- pe1/src/main/java/com/capgemini/pe1/AppCommun.java
+++ pe1/src/main/java/com/capgemini/pe1/AppCommun.java
@@ -3,8 +3,8 @@ package com.capgemini.pe1;
 public class AppCommun {
 
 	public static void main(String[] args) {
-		System.out.println("app commun abdallah");
-		System.out.println("app commun didier");
+		//System.out.println("app commun abdallah");
+		//System.out.println("app commun didier");
         System.out.println("correction bug en urgence");
 	}
 
